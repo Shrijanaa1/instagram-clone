af0042761c1e731fd2e6cbcade80bb1a09716ee9
bipin <bipin.mdev@gmail.com>
2020-09-09 14:09:23 +0545

    first git init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello cleaver programmers how you all doing</h1>
+    
+      <div className="app_header">
+        <img  className="app_headerImage" src="instagram_logo.png" alt=""/>
+      </div>
+      {/*header*/}
+      
+      {/*posts*/}
+      {/*posts*/}
+
     </div>
   );
 }
